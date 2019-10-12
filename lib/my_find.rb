b5d43ff53c5d95eaa7457b7ebd5f436724bbc1a0
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-12 04:49:10 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def my_find(collection)
-  i=0
+  i=0 #setting the counter
 
-  while i < collection.length
-    return collection[i] if yield(collection[i])
-    i+=1
+  while i < collection.length #while the counter is less than the length of our collection
+    return collection[i] if yield(collection[i]) #return the element in the collection if that element yielded to the block returns truthy
+    i+=1 #if it is not truthy, it won't break out of the loop, and the counter is incremented by 1! 
   end
 end
